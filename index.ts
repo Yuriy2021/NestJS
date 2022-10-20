2c6a64cc9c58c46cf99d39fe5bf5a29ec05288c8
Yuriy <xstream_06@mail.ru>
2022-10-20 14:29:14 +0300

    update

--- index.ts
+++ index.ts
@@ -13,3 +13,13 @@ window.addEventListener('DOMContentLoaded', () => {
       {name: 'Понял', handler: () => {console.log('Уведомление закрыто')}}
   )
 })
+window.addEventListener('DOMContentLoaded', () => {
+  renderUserBlock('0')
+  
+  renderSearchFormBlock()
+  renderSearchStubBlock()
+  renderToast(
+      {text: 'Это пример уведомления. Используйте его при необходимости', type: 'success'},
+      {name: 'Понял', handler: () => {console.log('Уведомление закрыто')}}
+  )
+})
