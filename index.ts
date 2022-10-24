37195e5efdf468b25c1856147cd3e4c1fd00079b
Yuriy <xstream_06@mail.ru>
2022-10-24 19:03:55 +0300

    HW_3Update

--- index.ts
+++ index.ts
@@ -12,3 +12,12 @@ window.addEventListener('DOMContentLoaded', () => {
       {name: 'Понял', handler: () => {console.log('Уведомление закрыто')}}
   )
 })
+window.addEventListener('DOMContentLoaded', () => {
+  renderUserBlock('0')
+  renderSearchFormBlock()
+  renderSearchStubBlock()
+  renderToast(
+      {text: 'Это пример уведомления. Используйте его при необходимости', type: 'success'},
+      {name: 'Понял', handler: () => {console.log('Уведомление закрыто')}}
+  )
+})
